c0f7baea3adf49346c332086317491cd7b0a184e
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-09-07 21:27:31 +1200

    create mock data to simulate notes data, map the data and display it by calling a post child component

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Note from './Note';
 
 const Notes = () => {
-  return <div></div>;
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: 'Build a cool todo App',
+      body:
+        'create a simple list of notes you can select and simple form that can be used for creating new notes or editing existing ones',
+    },
+    {
+      id: 2,
+      title: 'Write second article',
+      body:
+        'Write the second part of Loan Calculator tutorial and explain React-Hook-Forms features',
+    },
+  ]);
+  
+  return (
+    <div>
+      <h1>Notepad</h1>
+      {notes.map((note) => (
+        <Note key={note.id} title={note.title} body={note.body -m ""} />
+      ))}
+    </div>
+  );
 };
 
 export default Notes;
