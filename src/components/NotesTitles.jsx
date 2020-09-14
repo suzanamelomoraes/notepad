6531a656bb5166bca0c53477fc12aa726388c27b
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-09-14 14:23:03 +1200

    install uuid

--- src/components/NotesTitles.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NotesTitles = ({ notes }) => {
-  const noteTitles = notes.map((note) => <p key={note.id} >{note.title}</p>);
-
-  return <div>{noteTitles}</div>;
-};
-
-export default NotesTitles;
