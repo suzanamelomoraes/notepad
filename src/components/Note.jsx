accb15251863629debef2c6e0b4c5141068401fc
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-09-14 22:57:53 +1200

    include button to remove note and call removeNote method on the onClick

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import EditNote from './EditNote';
 
-const Note = ({ notes, editNote }) => {
+const Note = ({ notes, editNote, removeNote }) => {
   let { id } = useParams();
   let ID = Number(id);
 
@@ -15,11 +15,19 @@ const Note = ({ notes, editNote }) => {
 
   return (
     <div>
-      <h3>{noteToDisplay.title}</h3>
-      <p>{noteToDisplay.body}</p>
-
-      <button onClick={handleShowForm}>Edit note</button>
-      {showForm && <EditNote editNote={editNote} noteToEdit={noteToDisplay} />}
+      <div>
+        <h3>{noteToDisplay.title}</h3>
+        <p>{noteToDisplay.body}</p>
+      </div>
+      <div>
+        <button onClick={handleShowForm}>Edit note</button>
+        {showForm && (
+          <EditNote editNote={editNote} noteToEdit={noteToDisplay} />
+        )}
+      </div>
+      <div>
+        <button onClick={() => removeNote(noteToDisplay.id)}>Delete</button>
+      </div>
     </div>
   );
 };
