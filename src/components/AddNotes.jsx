11fe516cbfd6dcef6338a002ef7a19bbb1cd1ad6
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-09-14 14:15:57 +1200

    refactor form to receive only title and body as data and send it to the Notes parent

--- src/components/AddNotes.jsx
+++ src/components/AddNotes.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AddNotes = () => {
-  return <div></div>;
+const AddNotes = ({ addNote }) => {
+  const [value, setValue] = useState({
+    title: '',
+    body: '',
+  });
+
+  const handleInputChange = (e) => {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addNote(value);
+    setValue({
+      title: '',
+      body: '',
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='title'
+          className='input'
+          value={value.title}
+          placeholder='Title'
+          onChange={handleInputChange}
+        />
+        <input
+          type='text'
+          name='body'
+          className='input'
+          value={value.body}
+          placeholder='Note body...'
+          onChange={handleInputChange}
+        />
+        <input type='submit' className='button' />
+      </form>
+    </div>
+  );
 };
 
 export default AddNotes;
