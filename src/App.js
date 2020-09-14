9c89f8f01595d480e44d71cc5d5c34430563a87f
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-09-14 15:09:33 +1200

    import BrouserRouter and wrap Notes component to be able to use react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Notes from './components/Notes';
 
 function App() {
   return (
     <div className='App'>
-      <Notes />
+      <Router>
+        <Notes />
+      </Router>
     </div>
   );
 }
