74d06149d4f91d2ddb7f9c85c9c57f6645452d9d
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-09-14 16:17:59 +1200

    create link home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return (
-        <div className="header">
-            <h1>Notepad</h1>
-        </div>
-    )
-}
+  return (
+    <div className='header'>
+      <h1>Notepad</h1>
+      <p>
+        <Link to={'/'}>Home</Link>
+      </p>
+    </div>
+  );
+};
 
 export default Header;
